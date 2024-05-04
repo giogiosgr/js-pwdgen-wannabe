57dcf4f735a7cb86b68c503a82a3c38372ed592f
giogiosgr <giovannisgr88@gmail.com>
2024-05-04 21:47:25 +0200

    feat: bonus con layout responsive

--- BONUS/JS/main.js
+++ BONUS/JS/main.js
@@ -7,8 +7,15 @@ const favColor = prompt("Inserisci il tuo colore preferito");
 const firstNumber = prompt("Inserisci il primo numero");
 const secondNumber = prompt("Inserisci il secondo numero");
 
-// dichiarazione variabile con risultato della divisione dei due numeri di input, convertiti in Number
-const result = +firstNumber / +secondNumber
+/* dichiarazione variabile con risultato della divisione dei due numeri di input,
+convertiti in Number, arrotondati alla cifra minore */
+const result = Math.floor(+firstNumber / +secondNumber)
 
-// scrittura del risultato sulla console del browser  
-console.log(`${firstName}${lastName}${favColor}${result}`)
+// variabile contenente la password risultante
+const password = `${firstName}${lastName}${favColor}${result}`
+
+//inserimento del testo della password all'interno dell'elemento con id "password"
+const element = document.getElementById('password');
+element.innerText = password;
+
+
