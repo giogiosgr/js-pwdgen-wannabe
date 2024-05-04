04ff9edb407bf058d940326e9c757862b0996ddf
giogiosgr <giovannisgr88@gmail.com>
2024-05-04 16:52:04 +0200

    feat: versione base

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// dichiarazione e inizializzazione variabili tramite input 
+const firstName = prompt("Inserisci il tuo nome");
+const lastName = prompt("Inserisci il tuo cognome");
+const favColor = prompt("Inserisci il tuo colore preferito");
+
+// scrittura del risultato sulla console del browser  
+console.log(`${firstName}${lastName}${favColor}`)
